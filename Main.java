4f6b9ab346595b322032005adb44bc2a9a99105c
Abdelrahman <abdelrahman.y.salah@gmail.com>
2025-02-25 21:33:23 +0200

    Add Random Array Generation function

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+    public static ArrayList<Integer> getRandomIntegerArrayList(int size) {
+        ArrayList<Integer> Array = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            Array.add(random.nextInt());
+        }
+        return Array;
     }
 }
