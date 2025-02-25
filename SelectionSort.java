95fd5a0c2bbb4bfdddb5b1e3bf4ab9cd2bfaf99d
Abdelrahman <abdelrahman.y.salah@gmail.com>
2025-02-25 21:56:38 +0200

    Add Selection Sort Algrithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class SelectionSort {
+    public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            int idx = i;
+            for (int j = i ; j < list.size(); j++) {
+                if (list.get(j) < list.get(idx)) {
+                    idx = j;
+                }
+            }
+            int temp = list.get(idx);
+            list.set(idx, list.get(i));
+            list.set(i, temp);
+        }
+        System.out.println(list);
+        return list;
+    }
 }
