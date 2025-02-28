f31bacf2d3acfa7f9a6ac3fb8dd19c9ee8c88c65
Abdelrahman <abdelrahman.y.salah@gmail.com>
2025-02-28 23:06:19 +0200

    Final

--- Sort.java
+++ Sort.java
@@ -30,22 +30,14 @@ public abstract class Sort {
     }
 
     public static void insertionSort(int[] list) {
-        // Start with second element in the list and compare each element with all elements on the left side
         for (int i = 1; i < list.length; i++) {
             int num = list[i];
             int j = i - 1;
-            boolean swapped;
-            do {
-                if (num < list[j]) {
-                    swapped = true;
-                    int temp = num;
-                    list[j + 1] = list[j];
-                    list[j] = temp;
-                    j--;
-                } else {
-                    swapped = false;
-                }
-            } while (j >= 0 && swapped);
+            while (j >= 0 && list[j] > num) {
+                list[j + 1] = list[j];
+                j--;
+            }
+            list[j + 1] = num;
         }
     }
 
